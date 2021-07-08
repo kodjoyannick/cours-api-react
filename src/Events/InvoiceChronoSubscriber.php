7153362469ba5c515834c929f640497dbf8b455e
Yannick KKYS <kodjoyannick@gmail.com>
2021-07-08 08:54:55 +0100

    Modification sur le cycle de vie de la requête et extension de Doctrine

---
+++ src/Events/InvoiceChronoSubscriber.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Events;
+
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\Invoice;
+use App\Repository\InvoiceRepository;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\Security;
+
+class InvoiceChronoSubscriber implements EventSubscriberInterface
+{
+    private $security;
+    private $repository;
+
+    /**
+     * @param Security $security
+     * @param InvoiceRepository $repository
+     */
+    public function __construct(Security $security, InvoiceRepository $repository)
+    {
+        $this->security = $security;
+        $this->repository = $repository;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['setChronoForInvoice', EventPriorities::PRE_VALIDATE]
+         ];
+    }
+
+    public function setChronoForInvoice(ViewEvent $event)
+    {
+        $invoice = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+        
+        if ($invoice instanceof Invoice && $method === "POST") {
+            $nextChrono = $this->repository->findNextChrono($this->security->getUser());
+            $invoice->setChrono($nextChrono);
+
+            // TODO : A déplacer dans une classe dédiée
+            if (empty($invoice->getSentAt())) {
+                $invoice->setSentAt(new \DateTime());
+            }
+        }
+    }
+}
