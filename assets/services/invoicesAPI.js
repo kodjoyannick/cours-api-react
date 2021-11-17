464a3fe1a615ea3d0b707df0776702f51340e3ff
Yannick KKYS <kodjoyannick@gmail.com>
2021-11-17 22:13:21 +0100

    Centralisation de la configuration des adresses API

--- assets/services/invoicesAPI.js
+++ assets/services/invoicesAPI.js
@@ -1,30 +1,31 @@
 import axios from "axios";
+import { INVOICES_API } from "../config";
 
 function findAll() {
   return axios
-    .get("https://localhost:8000/api/invoices")
+    .get(INVOICES_API)
     .then((response) => response.data["hydra:member"]);
 }
 
 function deleteInvoice(id) {
-  return axios.delete("https://localhost:8000/api/invoices/" + id);
+  return axios.delete(INVOICES_API + "/" + id);
 }
 
 function find(id) {
   return axios
-    .get("https://localhost:8000/api/invoices/" + id)
+    .get(INVOICES_API + "/" + id)
     .then((response) => response.data);
 }
 
 function update(id, invoice) {
-  axios.put("https://localhost:8000/api/invoices/" + id, {
+  axios.put(INVOICES_API + "/" + id, {
     ...invoice,
     customer: `/api/customers/${invoice.customer}`,
   });
 }
 
 function create(invoice) {
-  axios.post("https://localhost:8000/api/invoices", {
+  axios.post(INVOICES_API, {
     ...invoice,
     customer: `/api/customers/${invoice.customer}`,
   });
